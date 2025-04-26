c004585451252cad21247155cef1ba9659e02c0e
Thilankavishka <thilankavishka25@gmail.com>
2025-04-26 15:52:45 +0530

    create Update Function

--- myapp/src/main/java/lk/ac/vau/fas/ict/Controllers/Controllers.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/Controllers/Controllers.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,7 @@ public class Controllers {
 	Student ob1 = new Student("2020ict30","Thilan",25,"IT",3.0);
 	Student ob2 = new Student("2020ict21","Sandeepa",25,"IT",6.0);
 	Student ob3 = new Student("2020ict49","goka",25,"IT",4.0);
-	Student ob4 = new Student("2020ict32","baiyamalliteshan",25,"IT",4.0);
+	Student ob4 = new Student("2020ict32","person",25,"IT",4.0);
 	//Student[] arr = {ob1,ob2,ob3,ob4};
 	/*@GetMapping("/list")
 	public Student[] getdetails() {
@@ -48,6 +50,17 @@ public class Controllers {
 		return "New Student Added";
 	}
 	
-	
+
+	//Update the student
+   @PutMapping("/update/{id}")
+   public String UpdateStudent(@PathVariable("id") String regNo,@RequestBody Student student)
+   {
+	   if(mstudent.get(regNo) != null)
+	   {
+		mstudent.put(student.getRegNo(), student);
+		   return "Update Successfully!";
+	   }
+	   return "404 Student not found";
+   }
 	
 }
